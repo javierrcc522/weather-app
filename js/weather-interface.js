2cf21b52527bdc719fb8e1ea57a567a9f7b9acac
javier jimenez and Sarah Lindmar <javier@>
2017-10-18 09:47:11 -0700

    make weather api calls using postman

---
+++ js/weather-interface.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c3b8c3628e862eeb95d36e2eb0af431a`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.")
+      }
+    });
+  });
+});
